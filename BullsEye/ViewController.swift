8bc421dd08dfb22789216add71a5dd2bf207eb6b
zhongwei li <lizhongwei.nkcs@gmail.com>
2015-10-29 12:52:28 +0800

    first commit

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var score = 0
+    var round = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var startOver: UIButton!
+    @IBOutlet weak var targetLable: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       // currentValue = lroundf(slider.value)
+       // targetValue = 1 + Int(arc4random_uniform(100))
+        startNewGame()
+        updateLabels()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable =
+                                trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable =
+                            trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +54,68 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showAlert() {
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+        
+        let title: String
+        if (difference == 0) {
+            title = "Perfect!"
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+      /*  let message = "The value of the slider is: \(currentValue)"
+                    + "\nThe target value is: \(targetValue)"
+                    + "\nThe difference is: \(difference)"
+                    + "\nYou scored \(points) points"
+        */
+        let message = "You scored \(points) points"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+                                                                            self.startNewRound()
+                                                                            self.updateLabels()
+        })
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+        
+        }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+      //  print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startOver(startOver: UIButton) {
+        startNewGame()
+        updateLabels()
+    }
 
+    func startNewRound() {
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50;
+        slider.value = Float(currentValue)
+    }
+    
+    func startNewGame() {
+        round = 0;
+        score = 0;
+        startNewRound()
+    }
+    
+    func updateLabels() {
+        targetLable.text = "\(targetValue)"
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
 }
 
